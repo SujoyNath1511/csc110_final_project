0dd24b457bca6d9dd4b6c4cb3f39441f0e429777
Yunjia Guo <70985365+yunjguo@users.noreply.github.com>
2020-12-10 16:09:50 -0500

    Add files via upload

--- visualization.py
+++ visualization.py
@@ -7,10 +7,11 @@ import plotly.graph_objects as go
 from typing import List
 
 
-def get_trace_first_point(x_value: list, y_value: list, mode: str, name: str) -> go.Scatter:
+def get_trace_first_point(x_value: list, y_value: list, mode: str, name: str, color: str) -> go.Scatter():
     """Return a go.Scatter object that only includes the first (x, y) point of given data.
        Set type (i.e. line or scatters) of the trace by the given string - mode.
        Set name, which will be shown in the legend, by the given string - name.
+       Set color of the trace by the given string - color.
        Result that is obtained by calling this function can be used for line chart animation.
 
        Preconditions:
@@ -19,13 +20,15 @@ def get_trace_first_point(x_value: list, y_value: list, mode: str, name: str) ->
            - mode in {'lines', 'markers', 'lines+markers'}
            - name != ''
     """
-    return go.Scatter(x=x_value[0:1], y=y_value[0:1], mode=mode, name=name)
+    return go.Scatter({'x': x_value[:1], 'y': y_value[:1], 'mode': mode, 'name': name,
+                       (mode[:-1] + '_color'): color})
 
 
-def get_trace_all_points(x_value: list, y_value: list, mode: str, name: str) -> go.Scatter:
+def get_trace_all_points(x_value: list, y_value: list, mode: str, name: str, color: str) -> go.Scatter():
     """Return a go.Scatter object that includes all points of given data.
        Set type (i.e. line or scatters) of the trace by the given string - mode.
        Set name, which will be shown in the legend, by the given string - name.
+       Set color of the trace by the given string - color.
 
        Preconditions:
            - x_value != []
@@ -34,7 +37,8 @@ def get_trace_all_points(x_value: list, y_value: list, mode: str, name: str) -> 
            - mode in {'lines', 'markers', 'lines+markers'}
            - name != ''
     """
-    return go.Scatter(x=x_value, y=y_value, mode=mode, name=name)
+    return go.Scatter({'x': x_value, 'y': y_value, 'mode': mode, 'name': name,
+                       (mode[:-1] + '_color'): color})
 
 
 def get_frames(x_value: list, y_value: List[list], mode: List[str], indexes: List[int]) -> list:
@@ -66,23 +70,24 @@ def get_frames(x_value: list, y_value: List[list], mode: List[str], indexes: Lis
     return frames
 
 
-def get_layout(frames: list, split_point: int, xrange: List[float], yrange: List[float]) -> go.Layout:
+def get_layout(frames: list, split_point: int, xrange: List[float], yrange1: List[float], yrange2: List[float]) \
+        -> go.Layout():
     """Return a go.Layout object, which can be passed to the attribute 'layout' of a go.Figure object.
        Through layout we can set interface arrangement and intersections for a graphic figure.
        The variable updatemenus in the function body is a list that can be passed to the attribute
        'updatemenus' for a go.Layout object.
 
-       The use for the given integer breakpoint is to divide analysis for previous data and prediction
+       The use for the given integer split_point is to divide analysis for previous data and prediction
        for future data. So it should be equal to the length of every aggregated dataset.
+       Each of the two given lists xrange and yrange should be a list containing two float values.
+       set range of xaxis and yaxis
 
        In updatemenus, we set:
-       A dropdown menu with three options:
-         - 'Temperature & Sea Level': to show the plot of all the data (temperature and sea level
-                                      versus time).
+       (i) Two buttons on the top:
          - 'Temperature vs Time': to show the plot of temperature versus time.
          - 'Sea Level vs Time': to show the plot of sea level versus time.
-       Three buttons:
-         - 'Clear': can be clicked to clear regression line(s) that has been generated.
+       (ii) Three buttons on the right:
+         - 'Clear': can be clicked to clear regression line(s) has been generated.
          - 'Analyze': can be clicked to show our animated regression line(s).
          - 'Predict': can be clicked to show our predicted data for future time, i.e. extended
                       regression line(s).
@@ -90,13 +95,35 @@ def get_layout(frames: list, split_point: int, xrange: List[float], yrange: List
     Preconditions:
         - breakpoint > 0
         - len(frames) >= breakpoint
-        - len(xrange) == len(yrange) == 2
-        - xrange[0] < xrange[1]
-        - yrange[0] < yrange[1]
+        - len(xrange) == len(yrange1) == len(yrange2) == 2
+        - all(range[0] < range[1] for range in [xrange, yrange1, yrange2])
     """
-    updatemenus = [dict(type='buttons',
+    updatemenus = [dict(type='buttons',  # set the two buttons on the top
+                        direction='right',
+                        showactive=False,
+                        x=0.8,
+                        y=1.11,
+                        xanchor='center',
+                        yanchor='top',
+                        buttons=[dict(label='Temperature vs Time',
+                                      method='update',
+                                      args=[dict(visible=[True, True, False, False]),
+                                            dict(title="Plot of Temperature versus Time",
+                                                 yaxis=dict(title='temperature (degree)',
+                                                            range=yrange1))]
+                                      ),
+                                 dict(label='Sea Level vs Time',
+                                      method='update',
+                                      args=[dict(visible=[False, False, True, True]),
+                                            dict(title="Plot of Sea Level versus Time",
+                                                 yaxis=dict(title='sea level (m)',
+                                                            range=yrange2))]
+                                      )
+                                 ]
+                        ),
+                   dict(type='buttons',  # set the three buttons on the right
                         showactive=False,  # not highlight the button
-                        x=1.1,  # set the position of the button
+                        x=1.09,  # set the position of the button
                         y=0.8,
                         xanchor='right',
                         yanchor='top',
@@ -119,40 +146,14 @@ def get_layout(frames: list, split_point: int, xrange: List[float], yrange: List
                                             args=[frames[:1]]
                                       )
                                  ]
-                        ),
-                   dict(type='dropdown',
-                        x=0.9,  # set the position of the dropdown menu
-                        y=1.1,
-                        xanchor='center',
-                        yanchor='top',
-                        buttons=[dict(label='Temperature & Sea Level',
-                                      method='update',
-                                      args=[dict(visible=[True, True, True, True]),
-                                            dict(yaxis=dict(title='temperature (degree) / sea level (m)',
-                                                            range=yrange))]
-                                      ),
-                                 dict(label='Temperature vs Time',
-                                      method='update',
-                                      args=[dict(visible=[True, True, False, False]),
-                                            dict(yaxis=dict(title='temperature (degree)',
-                                                            range=yrange))]
-                                      ),
-                                 dict(label='Sea Level vs Time',
-                                      method='update',
-                                      args=[dict(visible=[False, False, True, True]),
-                                            dict(yaxis=dict(title='sea level (m)',
-                                                            range=yrange))]
-                                      )
-                                 ]
                         )
                    ]
 
-    layout = go.Layout(title="Plots of Increasing Temperature/Sea level versus Time",  # title
+    layout = go.Layout(title="Plots of Temperature/Sea Level versus Time",  # title
                        hovermode='x unified',  # the mode of hover interactions
                        updatemenus=updatemenus,
                        xaxis=dict(title='time (year)',  # the label of x-axis
                                   range=xrange),  # fix the range of x-axis
-                       yaxis=dict(title='temperature (degree) / sea level (m)',
-                                  range=yrange),  # fix the range of x-axis
+                       yaxis=dict(range=[-10, 10])  # fix the range of x-axis
                        )
     return layout
