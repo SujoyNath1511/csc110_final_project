3ccda86703d7bc0a8e507b098f8d752b43c4e71f
20leebe <62720231+20leebe@users.noreply.github.com>
2020-12-09 23:59:03 -0500

    Update Datasets_aggregation.py
    Added the dataclass TempWaterInfo and changed the csv file functions so that they now convert it to a dict with the year as keys and TempWaterInfo as the mapping.

--- Datasets_aggregation.py
+++ Datasets_aggregation.py
@@ -13,41 +13,56 @@ If you guys think it is better, feel free to tell me and I will re-edit
 from typing import List, Tuple, Set, Any
 import csv
 
+@dataclass
+class TempWaterInfo:
+    """A dataclass that gives the temperature in celsius and average water level in meters for a specific year
 
-# I made these two functions specifically for the datasets that Benjamin Suggested
-def read_annual_mean_sea_level_new_zealand(filename: str) -> List[List[Any]]:
+    Instance Attributes:
+    - year: The year that the data was recorded for
+    - temp: The mean temperature for that given year
+    - sea_level: The average sea level for that given year in meters in different places
+    """
+    year: int
+    temp: float
+    sea_level: List[float]
+        
+
+def read_annual_mean_sea_level_new_zealand(filename: str, temp_info: Dict[int, TempWaterInfo]) -> None:
     """
     This function reads the csv file that stores the mean sea level relative to the land in New Zealand
 
     This function returns a list of lists that store all the rows in the csv file excluding the first row
     """
-    data = []
     with open(filename) as file:
         reader = csv.reader(file)
         next(reader)    # Skip the first row
         for row in reader:
-            data.append(row)
+            if row[2] == 'no_data' or int(row[0]) not in temp_info:
+                continue
 
-    return data
+            else:
+                year = int(row[0])
+                water_level = float(row[2])
+                temp_info[year].sea_level.append(water_level)
 
 
-def read_global_temp_new_zealand(filename: str) -> List[List[Any]]:
+def read_global_temp_new_zealand(filename: str) -> Dict[int, TempWaterInfo]:
     """
     This function reads the csv file that contains the New Zealand temperatures.
-
     It returns a list of lists that contain all the rows that have "NZ_7_Stn_Composite_Temp" excluding the first row
     """
-    data = []
+    data = {}
     with open(filename) as file:
         reader = csv.reader(file)
-        next(reader)    # skip the first row
+        next(reader)  # skip the first row
 
         for row in reader:
             if "NZ_7_Stn_Composite_Temp" in row:
-                data.append(row)
+                year = int(row[0])
+                temp = float(row[2])
+                data[year] = TempWaterInfo(year, temp, [])
 
     return data
-
 
 def read_general_csv_files(filename: str) -> List[List[Any]]:
     """
