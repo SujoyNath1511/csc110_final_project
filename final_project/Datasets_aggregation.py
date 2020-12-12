93edc37b9e011253884c29ffbab8f8ac1f576b32
20leebe <62720231+20leebe@users.noreply.github.com>
2020-12-12 16:52:23 -0500

    Update Datasets_aggregation.py

--- final_project/Datasets_aggregation.py
+++ final_project/Datasets_aggregation.py
@@ -1,21 +1,11 @@
 """
-Dummy functions that read csv datasets
 Datasets Aggregations
 
 CSC110 Final Project
-
-These are the functions that I came up with so far. If you guys have any suggestion or anything you want to point out,
-please do so.
-
-I did not find it necessary to apply classes here.
-If you guys think it is better, feel free to tell me and I will re-edit
 """
 from typing import List, Dict
 import csv
 from dataclasses import dataclass
-
-
-# I made these two functions specifically for the datasets that Benjamin Suggested
 
 
 @dataclass
@@ -23,47 +13,26 @@ class TempWaterInfo:
     """A dataclass that gives the temperature in celsius and average water level in meters for a specific year
 
     Instance Attributes:
-        - year: The year that the data was recorded for
-        - temp: The mean temperature for that given year
-        - sea_level: The average sea level for that given year in meters in different places
+        - years: A list of years that the data was recorded for
+        - temps: A list of temperatures in Celsius. The temperature at index i
+                 is the mean temperature for years[i]
+        - sea_levels: A list of average sea levels in meters. The sea level at index i is the mean temperature for
+                      years[i]
 
     Representation Invariant:
         - len(self.year) == len(self.temp) == len(self.sea_level)
     """
-    year: List[int]
-    temp: List[float]
-    sea_level: List[float]
-
-
-def read_annual_mean_sea_level_new_zealand(filename: str, temp_info: Dict[int, float]) \
-        -> Dict[str, TempWaterInfo]:
-    """
-    This function reads the csv file that stores the mean sea level relative to the land in New Zealand
-
-    This function returns a list of lists that store all the rows in the csv file excluding the first row
-    """
-    dict_so_far = {}
-    with open(filename) as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip the first row
-        for row in reader:
-            year = int(row[0])
-            if row[2] == 'no_data' or year not in temp_info:
-                continue
-            elif row[1] in dict_so_far:
-                dict_so_far[row[1]].year.append(year)
-                dict_so_far[row[1]].temp.append(temp_info[year])
-                dict_so_far[row[1]].sea_level.append(float(row[2]))
-            else:
-                dict_so_far[row[1]] = TempWaterInfo([year], [temp_info[year]], [float(row[2])])
-
-    return dict_so_far
+    years: List[int]
+    temps: List[float]
+    sea_levels: List[float]
 
 
 def read_global_temp_new_zealand(filename: str) -> Dict[int, float]:
     """
-    This function reads the csv file that contains the New Zealand temperatures.
-    It returns a list of lists that contain all the rows that have "NZ_7_Stn_Composite_Temp" excluding the first row
+    Returns a dictionary mapping years to temperatures for that year in New Zealand.
+    Reads the csv file temp_new_zealand.csv and filters out all the rows that don't have
+    'NZ_7_Stn_Composite_Temp' in the second column. Takes the first column as year and the 
+    third column as temperature.
     """
     data = {}
     with open(filename) as file:
@@ -77,3 +46,31 @@ def read_global_temp_new_zealand(filename: str) -> Dict[int, float]:
                 data[year] = temp
 
     return data
+
+
+def read_annual_mean_sea_level_new_zealand(filename: str, temp_info: Dict[int, float]) \
+        -> Dict[str, TempWaterInfo]:
+    """
+    Returns a dictionary with the location as the key and maps to TempWaterInfo, which stores the
+    temperature and sea level for that location for the given years. Reads the csv file
+    'annual-mean-sea-level-relative-to-land-19002013.csv'. It filters out years where there is no sea level
+    or temperature data for that given year. For the data not filtered out, it uses the second row as the
+    location and takes the sea level from the file. Uses temp_info to access the temperature for a given year.
+    
+    """
+    dict_so_far = {}
+    with open(filename) as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the first row
+        for row in reader:
+            year = int(row[0])
+            if row[2] == 'no_data' or year not in temp_info:
+                continue
+            if row[1] in dict_so_far:
+                dict_so_far[row[1]].years.append(year)
+                dict_so_far[row[1]].temps.append(temp_info[year])
+                dict_so_far[row[1]].sea_levels.append(float(row[2]))
+            else:
+                dict_so_far[row[1]] = TempWaterInfo([year], [temp_info[year]], [float(row[2])])
+
+    return dict_so_far
